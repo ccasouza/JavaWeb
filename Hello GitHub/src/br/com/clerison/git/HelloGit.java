60b10fd88593087bde1b41fbd1b195f757bd4a4c
Clérison Souza <60799279+ccasouza@users.noreply.github.com>
2020-09-17 01:56:31 -0300

    Add files via upload

---
+++ Hello GitHub/src/br/com/clerison/git/HelloGit.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.com.clerison.git;
+
+/**
+ * @author Daniele
+ *
+ */
+public class HelloGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Git!");
+
+	}
+
+}
